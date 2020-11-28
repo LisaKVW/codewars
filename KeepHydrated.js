4ebf533f26e23920e03aa9400c6966fc3c7c9742
Lisa Wand <lisavenneker21@gmail.com>
2020-11-28 15:46:11 -0500

    keep hydrated code wars resolved

---
+++ KeepHydrated.js
@@ -0,0 +1,19 @@
+function litres(time) {
+  return Math.floor(time / 2)
+}
+
+//Math.floor - rounds down
+// function litres(time) {
+//   let litres = Math.floor(time /2) 
+//   return litres;
+// }
+
+//1 hour = 0.5 liter water
+//3 = 1 liter
+//6.7 = 3 liter
+//11.8 = 5 liter
+
+//receive time in hours - return # of liters rounded down
+//time in hours not whole
+
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript 
