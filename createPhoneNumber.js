01a56e3c32454bbbd2a7928d4daa5fe7fd30cf7e
Lisa Wand <lisavenneker21@gmail.com>
2020-11-30 09:58:07 -0500

    create phone number codewars

---
+++ createPhoneNumber.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+function createPhoneNumber(numbers) {
+  let cleaned = ('' + numbers).replace(/\D/g, '')
+  //   /\D -Matches any character that is not a digit (Arabic numeral).
+  // /g for Do a global search for non-digit characters:
+  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  }
+  return null
+}
+
+
+//source on regex https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+//regex (cheatsheet) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+//create an array that accepts 10 integers
+//the return is a STRING
+//return must by in phone format "(xxx) xxx-xxxx"
