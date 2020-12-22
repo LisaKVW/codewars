90bd73f3c2bffe458e07dc11fa991a9783e0e5c6
Lisa Wand <lisavenneker21@gmail.com>
2020-12-21 20:22:53 -0500

    pseudo added

--- pythonwars/BorrowerSpeak.py
+++ pythonwars/BorrowerSpeak.py
@@ -3,11 +3,13 @@
 
 def borrow(s):
     punctuation = "!@#$%^&*()_+<>?:.,;"
-    no_punct = " "
-    for c in s:
+    no_punct = " "  # create empty string - this to push new string into - the result
+    for c in s:  # checking each character in string
+        # if character is not a punctuation then we create no_punct and add no_punct to characters (c)
         if c not in punctuation:
             no_punct = no_punct + c
-            lower = no_punct.lower()
+            lower = no_punct.lower()  # then we set our new string all to lowercase
+            # finally we remove the space between the words and return ONE string
             space = lower.replace(' ', '')
     return (space)
 
