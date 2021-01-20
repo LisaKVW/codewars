80bb9f96029fa9ba6b11b83e06e5ab8fb7247f58
Lisa Wand <lisavenneker21@gmail.com>
2021-01-20 11:57:37 -0500

    solution string match up bubble sort

--- StringMatchup.js
+++ StringMatchup.js
@@ -1,17 +1,36 @@
 //https://www.codewars.com/kata/59ca8e8e1a68b7de740001f4/train/javascript 
 
+// function solve(a, b) {
+//   let arrResult = [];
+//   let cont = 0;
+//   for (let i = 0; i < b.length; i++) {
+//     for (let j = 0; j < a.length; j++) {
+//       if (b[i] === a[j]) {
+//         cont++;
+//       }
+//     }
+//     arrResult.push(cont);
+//     cont = 0;
+//   }
+
+//   return arrResult;
+// }
+
+// const solve = (a, b) =>
+//   b.map(val => a.filter(v => v === val).length);
+
 function solve(a, b) {
   let result = []
-  let sum = ''
+  let sum = 0
   for (let i = 0; i < b.length; i++) {
-      if (a[i] === b[i]) {
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] === b[i]) {
         sum ++
-        result.push(sum)
-      } else {
-        sum = 0
-        result.push(sum)
-      }
     }
+  }
+    result.push(sum)
+    sum = 0
+}
   return result;
 }
 
