92153cd514f458292bb2ce2f14ba886759bd94e4
Lisa Wand <lisavenneker21@gmail.com>
2021-01-23 17:47:35 -0500

    trying with regex

--- SimpleLetterRemoval.js
+++ SimpleLetterRemoval.js
@@ -1,13 +1,7 @@
 //https://www.codewars.com/kata/5b728f801db5cec7320000c7/train/javascript 
 
 function solve(s, i) {
-  let result = s.split("")
-  for (let i = 0; i < result.length; i++) {
-    if (result[i] === 'a') {
-      result.shift()
-    }
-    return result.join()
-  }
+  s.replace(/[^a-zA-Z ]/g, "")
 }
 
 console.log(solve('abracadabra', 1))
@@ -19,6 +13,7 @@ console.log(solve('abracadabra', 1))
 // we gonna place string in an array via split()
 // after split - we want to remove the first a via shift()
 // join() makes array into string
+// filter()
 
 // MUST BE DONE BELOW
 //- first remove all letter 'a',
@@ -33,6 +28,7 @@ console.log(solve('abracadabra', 1))
 // Source: https://stackoverflow.com/questions/3568921/how-to-remove-part-of-a-string
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+//https://love2dev.com/blog/javascript-remove-from-array/
 
 //  TRIAL one works to delete first a
 // function solve(s, i) {
@@ -41,8 +37,22 @@ console.log(solve('abracadabra', 1))
 //     if (result[i] === 'a') {
 //       result.shift()
 //     }
-//     return result.join()
+//     return result.join('')
 //   }
 // }
 
-// console.log(solve('abracadabra', 1))
+// console.log(solve('abracadabra', 1))'
+
+//with filter
+
+// function solve(s, i) {
+//   let result = s.split("")
+//   // console.log(result)
+//   result.filter((letter, i, a) => {
+//     if (letter[i] === 'a') {
+//       result.splice(i, 5)
+//       i--
+//     }
+//   })
+//   return result
+// }
