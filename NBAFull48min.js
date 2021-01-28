8ef7a6d220f320c620a4773c7a06fcc34e409963
Lisa Wand <lisavenneker21@gmail.com>
2021-01-28 18:37:29 -0500

    solution NBAFull48min in js

--- NBAFull48min.js
+++ NBAFull48min.js
@@ -1,18 +1,20 @@
 // https://www.codewars.com/kata/587c2d08bb65b5e8040004fd/train/javascript
 
 function pointsPer48(ppg, mpg) {
-let result = (ppg/mpg)
-let final = ((result * 48))
-let rounded = final.toFixed(1)
-return rounded.replace(' ', '')
+  let result = (ppg / mpg)
+  let final = ((result * 48))
+  let rounded = Number(final.toFixed(1)) // toFixed - to get one decimal
+  if (isNaN(rounded)) rounded = 0  // this says if rounded is NOT a number then  rounded = 0
+  return rounded
 }
 
 console.log(pointsPer48(12, 20))
 console.log(pointsPer48(10, 10))
 console.log(pointsPer48(5, 17))
+console.log(pointsPer48(0, 0))
 
-// return how many point (ppg) are scored per 48min game?
-// ppg/mpg = point per game per minute of game = then to know based on 48 *48
+// return how many point (ppg) are scored per 48min game, rounded one decimal
+// ppg/mpg = point per game per minute of game = then to know based on 48min do * 48
 
 // sample pointsPer48(12, 20) // 28.8  - SO 12/20 = 0.6   - 0.6*48min = 28.8
 
