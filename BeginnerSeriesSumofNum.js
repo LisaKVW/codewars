eeed4a5f9598ca68b49eab23fa8363b8a7e72373
Lisa Wand <lisavenneker21@gmail.com>
2021-03-05 09:11:16 -0500

    working on beginner series, added for loop to get each num between a and b now need to sum them

--- BeginnerSeriesSumofNum.js
+++ BeginnerSeriesSumofNum.js
@@ -2,16 +2,32 @@
 // Beginner Series #3 Sum of Numbers
 
 function getSum(a, b) {
-  if( a === b){
-    return a
-  } else if (a !== b) {
-    return a + b
+  let arr = []
+  // let sum = ''
+  if (a === b){
+  return a
+} else if (a !== b) {
+  for (let i=a; i<=b; i++){
+    arr.push(i)
   }
 }
+return arr
+}
+
+
 
 //return sum of 2 integers a + b
 // if a && b are. === return a/b
-//The question is asking you to return the sum of ALL numbers in the RANGE of a and b .For example, if a = 1 and b = 5, then return 1+2+3+4+5.
+// if space between 2 numbers, get each individual number between the a and b, and sum these up as well
 
 console.log(getSum(0, -1));
 console.log(getSum(0, 1));
+console.log(getSum(174, 351));
+
+// function getSum(a, b) {
+//   if (a === b) {
+//     return a
+//   } else if (a !== b) {
+//     return a + b
+//   }
+// }
