7379551af98ffe54dfb5eb2c26033c770df09d67
Lisa Wand <lisavenneker21@gmail.com>
2021-03-05 19:19:14 -0500

    alost got beginner series - need work 2nd for loop

--- BeginnerSeriesSumofNum.js
+++ BeginnerSeriesSumofNum.js
@@ -3,26 +3,30 @@
 
 function getSum(a, b) {
   let arr = []
-  // let sum = ''
-  if (a === b){
-  return a
-} else if (a !== b) {
-  for (let i=a; i<=b; i++){
-    arr.push(i)
+  let sum = 0
+  if (a === b) {
+    return a
+  } else if (a !== b) {
+    for (let i = a; i <= b; i++) {
+      arr.push(i)
+    }
+    for (let j = 0; j < arr.length; j++) {
+      sum += arr[j]
+    }
   }
-}
-return arr
+  return sum
 }
 
-
+// the 2nd for loop is not gonna work cause we need starting point of a or b (which ever is smaller) - row 13 
+//close though!
 
 //return sum of 2 integers a + b
 // if a && b are. === return a/b
 // if space between 2 numbers, get each individual number between the a and b, and sum these up as well
 
-console.log(getSum(0, -1));
+console.log(getSum(0, -1));  // should give -1
 console.log(getSum(0, 1));
-console.log(getSum(174, 351));
+console.log(getSum(174, 200));
 
 // function getSum(a, b) {
 //   if (a === b) {
